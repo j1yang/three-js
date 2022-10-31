ff2c6a3bd50f93f77c3b0043f288af45f6f2684f
Jaewon Yang <106880500+j1yang@users.noreply.github.com>
2022-10-31 10:40:32 -0400

    Inspection function added

--- auto_compressor/main.py
+++ auto_compressor/main.py
@@ -4,6 +4,8 @@ import glob
 from turtle import width
 from venv import create
 import os
+import sys
+import pandas as pd
 
 
 def printOption(menu, min, max):
@@ -112,22 +114,103 @@ def resizeImages(inputFiles):
 
 
 def resize(width, height, inputFiles, folderPath):
+    outputFiles = []
     for i in range(len(inputFiles)):
         command = ["gltf-transform", "resize",
                    "--width", str(width), "--height", str(height), inputFiles[i], folderPath + '\\' + Path(str(inputFiles[i])).stem + '.glb', "--verbose"]
+        outputFiles.append(folderPath + '\\' +
+                           Path(str(inputFiles[i])).stem + '.glb')
         # display command
         print('\033[92m' + str(command) + '\033[0m')
         # run command
         subprocess.run(command, shell=True)
 
+    xlxsPath = str(Path(outputFiles[0]).parent) + "\inspection.xlsx"
+    addDataToExcel(outputFiles, xlxsPath)
+    print('\033[92m' + "Inspecting Files Completed! \nPath: " +
+          xlxsPath + '\033[0m')
 
-command = []
+
+def pd_toExcel(data, fileName):
+    name = []
+    mimeType = []
+    resolution = []
+    size = []
+    gpuSzie = []
+    for i in range(len(data)):
+        name.append(data[i][0]["name"])
+        mimeType.append(data[i][0]["mimeType"])
+        resolution.append(data[i][0]["resolution"])
+        size.append(data[i][0]["size"])
+        gpuSzie.append(data[i][0]["gpuSzie"])
+
+    dfData = {
+        'name': name,
+        'mimeType': mimeType,
+        'resolution': resolution,
+        'size(MB)': size,
+        'gpuSzie(MB)': gpuSzie
+    }
+
+    df = pd.DataFrame(dfData)
+    df.to_excel(fileName, index=False)
+
+
+def caculateValues(data, glbName):
+    index = len(data)
+    sumsize = 0
+    gpuSize = 0
+    mimeType = ""
+    resolution = ""
+    for i in range(0, index):
+        grid = data[i].split(',')
+        if grid[0] != '':
+            sumsize += float(grid[7])
+            gpuSize += float(grid[8])
+            if grid[6] > resolution:
+                resolution = grid[6]
+            if i == 0:
+                mimeType = grid[5]
+                resolution = grid[6]
+        else:
+            break
+
+    data = [{
+        'name': glbName,
+        'mimeType': mimeType,
+        'resolution': resolution,
+        'size': sumsize/1000000,
+        'gpuSzie': gpuSize/1000000
+    }]
+
+    return data
+
+
+def addDataToExcel(inputFiles, xlxsName):
+    datas = []
+    for i in range(len(inputFiles)):
+        command = ["gltf-transform", "inspect",
+                   inputFiles[i], "--format", "csv"]
+        result = subprocess.Popen(
+            command, shell=True, stdout=subprocess.PIPE, encoding="utf-8")
+        out, err = result.communicate()
+        result = out.splitlines()
+        # print(result)
+        for line in result:
+            if line == ' TEXTURES':
+                data = result[result.index(line)+3:result.index(" ANIMATIONS")]
+                # write_excel_xls("x.xls", "sheet1", value)
+                datas.append(caculateValues(data, inputFiles[i]))
+    pd_toExcel(datas, xlxsName)
+
 
 '''
 Automatic KTX2 Compressor
 '''
 
 # __MAIN__
+command = []
+
 
 # Get folder path that contains glb glbfiles
 while True:
@@ -154,16 +237,21 @@ print("\n")
 
 # Create output files names
 outputFiles = list(map(createOutputFile, inputFiles))
-
+xlxsPath = str(Path(outputFiles[0]).parent) + "\inspection.xlsx"
 match option:
     case 1:
         # Create output folder if doesn't exist
         Path(".\\" + inputPath + "_ktx2_etc1s").mkdir(parents=True, exist_ok=True)
         compressETC1S(inputFiles, outputFiles)
+        addDataToExcel(outputFiles, xlxsPath)
+        print('\033[92m' + "Inspecting Files Completed! \nPath: " +
+              xlxsPath + '\033[0m')
     case 2:
         # Create output folder if doesn't exist
         Path(".\\" + inputPath + "_ktx2_uastc").mkdir(parents=True, exist_ok=True)
-        compressUASTC(inputFiles, outputFiles)
+        compressUASTC(outputFiles, outputFiles)
+        print('\033[92m' + "Inspecting Files Completed! \nPath: " +
+              xlxsPath + '\033[0m')
     case 3:
         # resize
         resizeImages(inputFiles)
